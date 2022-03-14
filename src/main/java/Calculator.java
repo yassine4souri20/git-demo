c3dfb8162debec288a51f328236251fa3e8fbe04
yassine4souri20 <yassine.souri.20@gmail.com>
2022-03-14 18:00:28 +0100

    [feat]CALC-1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+
+    public int add(){
+
+        return 0;
+    }
+
+    public int sub(){
+
+        return 0;
+    }
+
+
+    public int mul(){
+
+        return 0;
+    }
+
+
+    public int adv(){
+
+        return 0;
+    }
+}
